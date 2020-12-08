6e458b5250dbf4542f0e0f7079129df7942932ab
Ferdhika Yudira <rpl4rt08@gmail.com>
2020-12-08 20:36:05 +0700

    :white_check_mark: add test race condition order product

---
+++ tests/Feature/Order/Order3Test.php
@@ -0,0 +1,41 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\Product;
+use App\Models\ProductStock;
+use Faker\Factory as Faker;
+use Tests\TestCase;
+
+class Order3Test extends TestCase
+{
+    public function testContainerGetWithAuth()
+    {
+        $user = User::factory(\App\Models\User::class)->create();
+        $product = Product::where('stock','!=',0)->first();
+
+        // time create order
+        // sleep(1);
+
+        // send request to api
+        $response = $this->call('POST', '/api/v1/orders', [
+            "user_id" => $user->id,
+            "note" => "Taro depan pintu aja",
+            "payment_time" => 2,// in second
+            "detail" => [
+                [
+                    "product_id" => $product->id,
+                    "quantity" => 1
+                ]
+            ]
+        ]);
+
+        // check product id stock 0
+        $this->assertDatabaseHas('products', [
+            'id' => $product->id,
+            'stock' => 0
+        ]);
+    }
+}
