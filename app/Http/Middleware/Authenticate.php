1f53543429a311a8ee4060ef336d58523b4b949b
Ferdhika Yudira <rpl4rt08@gmail.com>
2020-12-08 15:02:08 +0700

    :sparkles: except editorconfig, searchable param added

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,12 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            return response()->json([
+                'error' => [
+                    'code' => 401,
+                    'message' => "Unauthorized",
+                ]
+            ], 401);
         }
     }
 }
